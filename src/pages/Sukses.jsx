42a23b86b1f209be8fea76b25cf722fcfb64f544
anwarhakim31 <anwarhakim31@gmail.com>
2024-05-12 00:09:44 +0700

    Cashier Food (DONE)

--- src/pages/Sukses.jsx
+++ src/pages/Sukses.jsx
@@ -2,8 +2,8 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { Button } from "react-bootstrap";
 import Image from "react-bootstrap/Image";
-
-
+import logo from "../../assets/images/Succes.svg";
+import logo from "../../assets/images/Succes.svg";
 
 export default class Sukses extends Component {
   render() {
@@ -11,8 +11,8 @@ export default class Sukses extends Component {
       <div className="mt-4 text-center ">
         <h2>Sukses Pesan</h2>
         <h2>Terima Kasih Sudah Memesan</h2>
-        <Image src="images/Succes.svg" width="200" className="d-block m-auto" />
-
+        <Image src={logo} width="200" className="d-block m-auto" />
+        <Image src={logo} width="200" className="d-block m-auto" />
         <Button className="theme " as={Link} to="/">
           Kembali
         </Button>

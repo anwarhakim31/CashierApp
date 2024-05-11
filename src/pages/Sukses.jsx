8d28b88189e380b804a3e221cf8d5a8ef93425e5
AnwarHakim <anwarhakim31@gmail.com>
2024-05-11 23:50:52 +0700

    Update Sukses.jsx

--- src/pages/Sukses.jsx
+++ src/pages/Sukses.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { Button } from "react-bootstrap";
 import Image from "react-bootstrap/Image";
-import logo from "../../assets/images/Succes.svg";
+
 
 
 export default class Sukses extends Component {
@@ -11,7 +11,7 @@ export default class Sukses extends Component {
       <div className="mt-4 text-center ">
         <h2>Sukses Pesan</h2>
         <h2>Terima Kasih Sudah Memesan</h2>
-        <Image src={logo} width="200" className="d-block m-auto" />
+        <Image src="../../public/images/Succes.svg" width="200" className="d-block m-auto" />
 
         <Button className="theme " as={Link} to="/">
           Kembali
